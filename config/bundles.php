b2c1cfbf50645abd014a77baeaa645dd925d311e
DRACOX <alexis.forestier2001@gmail.com>
2022-12-23 15:55:33 +0100

    allow CORS

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
