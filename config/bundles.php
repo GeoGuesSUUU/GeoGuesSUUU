2b9ddfea6dfb5ade19083fe4a42acf33a5cb8e4d
DRACOX <alexis.forestier2001@gmail.com>
2022-12-23 17:07:22 +0100

    remove cors bundle (de la grosse merde)

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
