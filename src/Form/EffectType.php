0eb2c881e1280424217bd7a0adbaee618f10d882
Erwan <erwan.gerba@gmail.com>
2023-01-09 11:27:21 +0100

    add effect form

---
+++ src/Form/EffectType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Effect;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EffectType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('value')
+            ->add('type', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices' => [
+                    'life-' => 'life-',
+                    'shield-' => 'shield-',
+                    'price-' => 'price-',
+                    'claim-' => 'claim-',
+                    'life+' => 'life+',
+                    'life_max+' => 'life_max+',
+                    'shield+' => 'shield+',
+                    'shield_max+' => 'shield_max+',
+                    'price+' => 'price+',
+                    'claim+' => 'claim+',
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
