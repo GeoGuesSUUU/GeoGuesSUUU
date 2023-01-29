c2b80d8ec12f94b1705783ff0aec1446c769f1f4
DRACOX <alexis.forestier2001@gmail.com>
2023-01-30 00:19:41 +0100

    fix level system

--- src/Utils/LevelManager.php
+++ src/Utils/LevelManager.php
@@ -4,19 +4,19 @@ namespace App\Utils;
 
 class LevelManager
 {
-    public const LEVEL_MULTIPLIER = 1.5;
+    public const LEVEL_MULTIPLIER = 0.9;
 
-    public const LEVEL_1_XP = 1500;
+    public const LEVEL_1_XP = 1000;
 
     public static function getLevelByXp(int $xp): int
     {
-        return round($xp / (LevelManager::LEVEL_1_XP * LevelManager::LEVEL_MULTIPLIER), 0, PHP_ROUND_HALF_DOWN);
+        return floor(pow($xp / LevelManager::LEVEL_1_XP, LevelManager::LEVEL_MULTIPLIER));
     }
 
     public static function getXpLevel(int $level): int
     {
         if ($level <= 0) return 0;
         if ($level === 1) return self::LEVEL_1_XP;
-        return round(self::LEVEL_1_XP * self::LEVEL_MULTIPLIER * $level);
+        return round(pow($level, 1 / LevelManager::LEVEL_MULTIPLIER) * LevelManager::LEVEL_1_XP);
     }
 }
